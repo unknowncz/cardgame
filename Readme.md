0d820679d4f5caaee93e81797f42631079885421
unknowncz <86740586+unknowncz@users.noreply.github.com>
2021-12-24 10:51:58 +0100

    Added files

---
+++ Readme.md
@@ -0,0 +1,35 @@
+# Commands #
+
+## !shuffle ##
+
+Shuffles the deck of cards regardless wether it was already shuffled or not.¨
+
+Usage: !shuffle
+
+## !sort ##
+
+As the name suggests this command sorts the deck.
+
+Usage: !sort
+
+## !select ##
+
+Select the card to find in the deck.
+
+Usage: !select x (!select 5)
+
+## !play ##
+
+Starts the automated process of drawing cards. When the selected card is found you win.
+
+Usage: !play
+
+## !exit ##
+
+Exits.
+
+Usage: !exit
+
+***
+
+The deck consists of numbers ranging from 1 to 32.
---
+++ main.py
@@ -0,0 +1,51 @@
+import random,time,termcolor
+class deck:
+    def __init__(self,cards):self.cards=cards
+    def shuffle(self,*overflow):self.cards=random.sample(self.cards,len(self.cards))
+    def sort(self,*overflow):self.cards.sort()
+    def selectcard(self,card,*overflow):
+        try:
+            if card[0]in self.cards:self.selected=card[0]
+            else:print('Card not in deck')
+        except IndexError:pass
+    def checknext(self):
+        try:
+            if self.selected in self.cards:
+                if (c:=self.cards.pop(0))==self.selected:
+                    self.cards.append(c)
+                    termcolor.cprint(c, 'green')
+                    return True
+                termcolor.cprint(c, 'red')
+                self.cards.append(c)
+            return False
+        except AttributeError:
+            print('No card selected')
+            return True
+
+def main():
+    karty=[str(i+1)for i in range(32)]
+    Deck=deck(karty)
+    def play(*overflow):
+        while True:
+            if Deck.checknext():break
+            time.sleep(1)
+    def passingfunc(*overflow):pass
+    def end(*overflow):return True
+
+    commands={
+        '!shuffle':Deck.shuffle,
+        '!sort':Deck.sort,
+        '!select':Deck.selectcard,
+        '!play':play,
+        '!exit':end
+    }
+
+    while True:
+        try:s=input('command: ')
+        except KeyboardInterrupt:exit()
+        if commands.get(s.split(' ')[0],passingfunc)(s.split(' ')[1:]or['']):break
+    Deck.sort()
+    print(*Deck.cards)
+    input()
+
+if __name__=='__main__':main()
